6fadafa0af7b000345905b34ecfff9f3621d5e79
zhnlk <tomleader0828@gmail.com>
2021-04-16 17:35:37 +0800

    1 and 2 done

--- quizz_solutions/1.py
+++ quizz_solutions/1.py
@@ -1,2 +1,56 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# author: zhnlk
 
-## 用TA-Lib中的MACD技术指标，根据最新的股票池数据，选出指标中MACD值向上突破0的股票池
+"""
+用TA-Lib中的MACD技术指标，根据最新的股票池数据，选出指标中MACD值向上突破0的股票池
+要点提炼：
+1. 使用ta-lib的MACD指标
+2. 根据最新的股票池
+3. MACD值向上突破的股票池
+
+
+'''
+MACD算法：（异同移动平均线 Moving Average Convergence / Divergence） Gerald Appel@1979-02-04
+MACD=2*(DIF-DEA)
+
+利用收盘价的快慢线之间的聚合与分离，对买卖时机做出研判的技术指标
+1.计算EMA
+    EMA(12) = 前一日EMA（12）×11/13+今日收盘价×2/13, 快线：快速移动平均线,一般取12日,
+    EMA(26) = 前一日EMA（26）×25/27+今日收盘价×2/27, 慢线：慢速移动平均线,一般取26日
+2.计算DIF
+    DIF,差离值：EMA(12)-EMA(26)
+3.计算IDF的9日EMA
+    根据离差值计算其9日的EMA，即离差平均值，是所求的MACD值。为了不与指标原名相混淆，此值又名DEA或DEM
+    今日DEA(MACD) = 前一日DEA×8/10+今日DIF×2/10
+    计算出的DIF和DEA的数值均为正值或负值, 用(DIF-DEA)*2即为MACD柱状图。
+'''
+"""
+
+import pandas as pd
+import talib
+
+from mootdx.quotes import Quotes
+
+sz_50_csv = "../data/sz50_stocks.csv"
+
+
+def get_macd(code: str):
+    client = Quotes.factory(market='std')
+    k_line = client.bars(symbol=code, frequency=9, offset=100)
+    macd, signal, hist = talib.MACD(k_line['close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    if macd[-1:].values[0] > 0:
+        return code
+
+
+def stock_pool():
+    sz50 = pd.read_csv(sz_50_csv)
+
+    for s in [str(i[0]) for i in sz50.values]:
+        ss = get_macd(s)
+        if ss:
+            print(ss)
+
+
+if __name__ == '__main__':
+    stock_pool()
--- quizz_solutions/2.py
+++ quizz_solutions/2.py
@@ -1,4 +1,54 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 # author: zhnlk
-# 利用个股一分钟数据，写一个合成任意分钟的通用函数，使用000001.SZ平安银行的数据。
+
+"""
+利用个股一分钟数据，写一个合成任意分钟的通用函数，使用000001.SZ平安银行的数据。
+
+要点提炼：
+1. 获取数据
+2. 合成K线
+3. pd与np的使用
+"""
+
+import numpy as np
+import pandas as pd
+from mootdx.quotes import Quotes
+
+
+def get_1min_k_line() -> pd.DataFrame:
+    client = Quotes.factory(market='std')
+    k_line = client.bars(symbol='000001', frequency=7, offset=100)
+    return k_line
+
+
+def generate_n_min_k(df: pd.DataFrame, n: int) -> pd.DataFrame:
+    data_list = []
+    for i in range(0, df.index.size, n):
+        _high = max(df.iloc[i:i + n]['high'])
+        _low = min(df.iloc[i:i + n]['low'])
+        _open = df.iloc[i]['open']
+        _close = df.iloc[i]['close']
+        _vol = sum(df.iloc[i:i + n]['vol'])
+        _amount = sum(df.iloc[i:i + n]['amount'])
+        _year = df.iloc[i]['year']
+        _month = df.iloc[i]['month']
+        _day = df.iloc[i]['day']
+        _hour = df.iloc[i + n - 1]['hour']
+        _minute = df.iloc[i + n - 1]['minute']
+        _datetime = df.iloc[i + n - 1]['datetime']
+        data_list.append([_open, _close, _high, _low, _vol, _amount, _year, _month, _day, _hour, _minute, _datetime])
+
+    dff = pd.DataFrame(np.array(data_list),
+                       columns=df.columns)
+    return dff
+
+
+def generate_5min_k(df: pd.DataFrame):
+    print(generate_n_min_k(df, 5))
+
+
+if __name__ == '__main__':
+    k_1min = get_1min_k_line()
+    print(k_1min)
+    generate_5min_k(k_1min)
